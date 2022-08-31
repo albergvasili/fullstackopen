5a402c1bcaf12e650cedc2305832abc6b2bf2c23
albergvasili <albergvasili@yandex.com>
2022-08-31 19:06:11 -0400

    Store course part object literals in an array
    Exercise 1.4 courseinfo step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,20 +1,20 @@
 const App = () => {
   const course = 'Half Stack application development';
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
 
   const Header = (props) => {
     return (
@@ -33,9 +33,9 @@ const App = () => {
   const Content = () => {
     return (
       <div id="content">
-        <Part part = {part1} />
-        <Part part = {part2} />
-        <Part part = {part3} />
+        <Part part = {parts[0]} />
+        <Part part = {parts[1]} />
+        <Part part = {parts[2]} />
       </div>
     )
   };
@@ -43,7 +43,7 @@ const App = () => {
   const Total = () => {
     return (
       <p>
-        Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+        Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises} 
       </p>
     )
   };
