7de2308ccc7281109af61da7184354b5fd70d937
albergvasili <albergvasili@yandex.com>
2022-08-31 18:01:05 -0400

    Store course part data in object literals
    Exercise 1.3 courseinfo step3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,11 +1,20 @@
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
 
   const Header = (props) => {
     return (
@@ -16,7 +25,7 @@ const App = () => {
   const Part = (props) => {
     return (
         <p>
-          {props.part} {props.exercise}
+          {props.part.name} {props.part.exercises}
         </p>
     )
   };
@@ -24,9 +33,9 @@ const App = () => {
   const Content = () => {
     return (
       <div id="content">
-        <Part part = {part1} exercise = {exercises1} />
-        <Part part = {part2} exercise = {exercises2} />
-        <Part part = {part3} exercise = {exercises3} />
+        <Part part = {part1} />
+        <Part part = {part2} />
+        <Part part = {part3} />
       </div>
     )
   };
@@ -34,7 +43,7 @@ const App = () => {
   const Total = () => {
     return (
       <p>
-        Number of exercises {exercises1 + exercises2 + exercises3}
+        Number of exercises {part1.exercises + part2.exercises + part3.exercises}
       </p>
     )
   };
