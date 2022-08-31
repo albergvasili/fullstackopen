2531f2a0a6c5fd836365590d241efd6d5cf38975
albergvasili <albergvasili@yandex.com>
2022-08-31 17:59:12 -0400

    Refactor Content component
    Exercise 1.2 courseinfo, step 2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,18 +13,20 @@ const App = () => {
     )
   };
 
+  const Part = (props) => {
+    return (
+        <p>
+          {props.part} {props.exercise}
+        </p>
+    )
+  };
+
   const Content = () => {
     return (
       <div id="content">
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
+        <Part part = {part1} exercise = {exercises1} />
+        <Part part = {part2} exercise = {exercises2} />
+        <Part part = {part3} exercise = {exercises3} />
       </div>
     )
   };
