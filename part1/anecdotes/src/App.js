ed887c9a58b7e5848c0a346c9e8c66497d3cc8d8
albergvasili <albergvasili@yandex.com>
2022-09-08 18:30:28 -0400

    Copy starter code

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import {useState } from 'react';
 
 const App = () => {
+  const anecdotes = [
+    "Every now and then the other one suffers.",
+    "Always do it better.",
+    "We must learn the hard way. Stop teaching!",
+    "There is no better training than fighting for survive.",
+    "If you don't eat, you don't work.",
+    "By doing nothing, everything begins to happen.",
+    "In the end, everything is just furniture."
+  ];
+
+  const [selected, setSelected] = useState(0);
+
   return (
+    <div>
+      {anecdotes[selected]}
+    </div>
   )
 };
 
