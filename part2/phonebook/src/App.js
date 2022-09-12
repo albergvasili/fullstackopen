ffca2be7a1e94478dc2b5e21ef7180d6147ed119
albergvasili <albergvasili@yandex.com>
2022-09-12 18:01:41 -0400

    Copy starter code

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Alberg Vasili' }
+  ]);
+  const [newName, setNewName] = useState('');
+
   return (
-    <div> </div>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    <h2>Numbers</h2>
+    </div>
   )
 };
 export default App;
