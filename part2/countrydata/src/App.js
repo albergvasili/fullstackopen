09cc0b279f3c32edeac0604eeb5cd8688f13d404
albergvasili <albergvasili@yandex.com>
2022-09-19 17:48:03 -0400

    Write country input as a controlled component

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -1,11 +1,19 @@
-import { useState, setState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [country, setCountry] = useState('');
+
+  const changeCountry = (event) => {
+    setCountry(event.target.value)
+  }
 
   return(
     <>
       <h1>Country Data</h1>
-      <p>Find country: <input type="text" /></p>
+      <p>
+        Find country:
+        <input value={country} onChange={changeCountry} type="text" />
+      </p>
     </>
   )
 };
