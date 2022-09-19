40d098d04b7026e19597fa8a97da2394705fb5ad
albergvasili <albergvasili@yandex.com>
2022-09-19 19:06:45 -0400

    Handle multiple search results
    -Create result Hook State
    -Extract Result component
    -Show results when at least 10 countries match the query

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -12,9 +12,23 @@ const Search = ({ find, value, onChange }) => {
   )
 };
 
+const Result = ({ result }) => {
+    if (result.length >= 10){
+      return <p>Too many matches, specify another filter</p>
+    } else if (result.length > 1) {
+      return(
+        <div>
+          {result.map(
+            country => <p key={country.name.common}>{country.name.common}</p>
+          )}
+        </div>
+      )
+  }
+};
 
 const App = () => {
   const [country, setCountry] = useState('');
+  const [result, setResult] = useState({});
 
   useEffect(() => {
     if (country){
@@ -22,6 +36,7 @@ const App = () => {
         .get(`https://restcountries.com/v3.1/name/${country}`)
         .then(response => {
           console.log(response)
+          setResult(response.data)
         })
     }
   },[country])
@@ -34,8 +49,7 @@ const App = () => {
     <>
       <h1>Country Data</h1>
       <Search find='country' value={country} onChange={changeCountry} />
-      <h2>Country</h2>
-      <p></p>
+      <Result result={result}/>
     </>
   )
 };
