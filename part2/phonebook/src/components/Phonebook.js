b66502a585f7b99db45e93984c633adc5b82767e
albergvasili <albergvasili@yandex.com>
2022-09-26 13:38:45 -0400

    Add a 'delete' button to  each contact

--- part2/phonebook/src/components/Phonebook.js
+++ part2/phonebook/src/components/Phonebook.js
@@ -1,22 +1,37 @@
 /* Phonebook and ContactList components */
 
-const Phonebook = ({ filter, fullList, filteredList }) => {
+const Phonebook = ({ filter, fullList, filteredList, onClick }) => {
   if (!filter) {
     return (
-      <ContactList phoneList={filteredList} />
+      <ContactList phoneList={filteredList} onClick={onClick}/>
     )}
   return (
-      <ContactList phoneList={fullList} />
+      <ContactList phoneList={fullList} onClick={onClick}/>
   )
 }
 
-const ContactList = ({ phoneList }) => {
+const ContactList = ({ phoneList, onClick }) => {
   return (
     <>
       {phoneList.map(
-        person => <p key={person.name}>{person.name} - {person.number}</p>
+        person => <Entries key={person.id} 
+                    person={person}
+                    onClick={onClick}/>
       )}
     </>
+  )
+};
+
+const Entries = ({ person, onClick }) => {
+  return (
+  <>
+    <p key={person.name}>
+      {person.name} - {person.number}
+      <button type="submit" value={person.id}  onClick={onClick}>
+        Delete
+      </button>
+    </p>
+  </>
   )
 };
 
