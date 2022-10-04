767d351948cea5f73b36c9f462358f5c007ec2ed
albergvasili <albergvasili@yandex.com>
2022-10-04 18:53:16 -0400

    Add phonebook entries

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const data = [
+  {
+    "id": 1,
+    "name": "Alberg Vasili",
+    "number": "123-12334"
+  },
+  {
+    "id": 2,
+    "name": "Luna Greenleaf",
+    "number": "223-15334"
+  },
+  {
+    "id": 3,
+    "name": "Monsieur Monkey",
+    "number": "223-35038"
+  },
+  {
+    "id": 4,
+    "name": "Akesi Jelo",
+    "number": "423-25238"
+  }
+];
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
