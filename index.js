28bffa46b362e3379198e598dbbfe92df20de851
albergvasili <albergvasili@yandex.com>
2022-10-11 19:06:59 -0400

    Install CORS
    -Front-end runs on port 3000 and backend on port 3001
    
    Exercise 3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(morgan(':method :url :status - :response-time ms - :data'));
 
