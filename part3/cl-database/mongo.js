e1936a76e25b8326ad7fb66b62e45f08902c8d5e
albergvasili <albergvasili@yandex.com>
2022-10-21 18:46:13 -0400

    Print all entries when no arguments are provided
    -Check if there are 3 arguments to display phonebook
    
    Exercise 3.12 Command-line database

--- part3/cl-database/mongo.js
+++ part3/cl-database/mongo.js
@@ -11,22 +11,37 @@ const schema = new mongoose.Schema({
 
 const Entry = mongoose.model('Entry', schema);
 
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected');
-
-    const entry = new Entry({
-      name: process.argv[3],
-      number: process.argv[4]
+if (process.argv.length === 3) {
+  mongoose
+    .connect(url)
+    .then(() => {
+      Entry.find({}).then(res => {
+        console.log('Phonebook:');
+        res.forEach(entry => {
+          console.log(entry.name, entry.number);
+        })
+      })
+     mongoose.connection.close()
     });
+} else {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      console.log('connected');
 
-    return entry.save();
-  })
-  .then(() => {
-    console.log(
-      `Added ${process.argv[3]} number ${process.argv[4]} to phonebook`
-  );
-    return mongoose.connection.close()
-  })
-  .catch((error) => console.log(error))
+
+      const entry = new Entry({
+        name: process.argv[3],
+        number: process.argv[4]
+      });
+
+      return entry.save();
+    })
+    .then(() => {
+      console.log(
+        `Added ${process.argv[3]} number ${process.argv[4]} to phonebook`
+    );
+      return mongoose.connection.close()
+    })
+    .catch((error) => console.log(error))
+};
