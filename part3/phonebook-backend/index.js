58741dd917880b58950d0a3f138c694213b1b265
albergvasili <albergvasili@yandex.com>
2022-11-10 19:00:08 -0500

    Fix all the syntax warnings using ESlint
    Exercise 3.22 Lint configuration

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use(morgan(':method :url :status - :response-time ms - :data'));
 
-morgan.token('data', (req, res) => {
+morgan.token('data', (req) => {
   if (req.method === 'POST') {
     return JSON.stringify(req.body);
   } else {
@@ -23,7 +23,7 @@ app.get('/api/persons', (req, res) => {
   Entry.find({})
     .then(result => {
       res.json(result);
-    })
+    });
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
@@ -36,28 +36,29 @@ app.get('/api/persons/:id', (req, res, next) => {
       res.status(400).json(id);
     }
   })
-  .catch(error => next(error))
+    .catch(error => next(error));
 });
 
 app.get('/info', (req, res) => {
   const now = new Date();
 
-  Entry.find({}).then(result => {
-    const amount = result.length;
-    res.send(`
-      <p>Phonebook has info for ${amount} people</p>
-      <p>${now}</p>
-      `);
-  })
+  Entry.find({})
+    .then(result => {
+      const amount = result.length;
+      res.send(`
+        <p>Phonebook has info for ${amount} people</p>
+        <p>${now}</p>
+        `);
+    });
 });
 
 app.post('/api/persons', (req, res, next) => {
   const body = req.body;
 
-  Entry.find({name: body.name})
+  Entry.find({ name: body.name })
     .then(result => {
       if (result.length !== 0) {
-        res.status(404).send('Duplicate data')
+        res.status(404).send('Duplicate data');
       } else {
         const entry = new Entry({
           name: body.name,
@@ -66,11 +67,12 @@ app.post('/api/persons', (req, res, next) => {
 
         entry.save()
           .then(savedEntry => {
-          res.json(savedEntry);
-        })
+            res.json(savedEntry);
+          })
           .catch(error => next(error));
       }
-  }).catch(error => next(error));
+    })
+    .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -88,29 +90,29 @@ app.put('/api/persons/:id', (req, res, next) => {
     .then(returnedEntry =>
       res.json(returnedEntry)
     )
-    .catch(error => next(error))
+    .catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Entry.findByIdAndRemove(req.params.id)
     .then(result => {
       if (result) {
-      res.status(204).end()
+        res.status(204).end();
       }
     })
-    .catch((error) => next(error))
+    .catch((error) => next(error));
 });
 
 const errorHandler = (error, req, res, next) => {
   console.error(error.message);
 
   if (error.name === 'CastError') {
-    return res.status(400).send(error.message)
+    return res.status(400).send(error.message);
   } else if (error.name === 'ValidationError') {
-    return res.status(400).send(error.message)
+    return res.status(400).send(error.message);
   }
 
-  next(error)
+  next(error);
 };
 
 app.use(errorHandler);
--- part3/phonebook-backend/models/entry.js
+++ part3/phonebook-backend/models/entry.js
@@ -5,12 +5,12 @@ const url = process.env.MONGODB_URI;
 console.log('Connecting to database');
 
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch(error => {
     console.log('Error connecting to MongoDB:', error.message);
-  })
+  });
 
 const schema = new mongoose.Schema({
   name: {
@@ -31,10 +31,10 @@ const schema = new mongoose.Schema({
 
 schema.set('toJSON', {
   transform: (document, returnedObject) => {
-  returnedObject.id = returnedObject._id.toString()
-  delete returnedObject._id
-  delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-  })
+});
 
 module.exports = mongoose.model('Entry', schema);
