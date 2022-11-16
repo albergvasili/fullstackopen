48ee2bdbc378d3c3e9a162f162e4cc2704a5ca1b
albergvasili <albergvasili@yandex.com>
2022-11-15 19:16:25 -0500

    Create 'controllers/blog' module
    -Extract routes from index.js
    -Use express router

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -6,29 +6,16 @@ const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 const Blog = require('./models/blog');
+const blogRouter = require('./controllers/blog');
+
+
+app.use('/api/blogs', blogRouter);
 
 mongoose.connect(config.mongoURL);
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result);
-    });
-});
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
