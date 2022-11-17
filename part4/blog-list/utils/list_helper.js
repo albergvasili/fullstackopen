451337e3558aaa4b5ca8a9e9bbec4d19645497ae
albergvasili <albergvasili@yandex.com>
2022-11-17 18:40:38 -0500

    Define totalLikes function
    -Use the reduce() method to return the sum of likes

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, blogs) => {
+    return accumulator + blogs.likes;
+  };
+
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
