578a36afb98e599bd6fb64176b2dcee056f07570
albergvasili <albergvasili@yandex.com>
2022-11-17 18:50:42 -0500

    Test 'totalLikes' function (single blog list)

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -8,8 +8,20 @@ test('dummy returns one', () => {
 
 describe('total likes', () => {
   const emptyList = [];
+  const singleBlogList = [
+    {
+      title: 'Loneliness',
+      authors: 'Sola No',
+      url: 'Ouais',
+      likes: 9
+    }
+  ];
 
   test('of empty list is zero', () => {
     expect(listHelper.totalLikes(emptyList)).toBe(0);
+  });
+
+  test('of single blog list is the likes of that single blog', () => {
+    expect(listHelper.totalLikes(singleBlogList)).toBe(9);
   });
 });
