2814a316f8d9a388ba277fdec01b5ef7e5baf3df
albergvasili <albergvasili@yandex.com>
2022-11-17 18:53:53 -0500

    Test 'totalLikes' function (multiple blogs list)
    Exercise 4.4 helper functions and unit tests step2

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -16,6 +16,20 @@ describe('total likes', () => {
       likes: 9
     }
   ];
+  const blog = [
+    {
+      title: 'String',
+      authors: 'String',
+      url: 'String',
+      likes: 3
+    },
+    {
+      title: 'Guten tag',
+      authors: 'Tralali',
+      url: 'Line',
+      likes: 7
+    }
+  ];
 
   test('of empty list is zero', () => {
     expect(listHelper.totalLikes(emptyList)).toBe(0);
@@ -23,5 +37,8 @@ describe('total likes', () => {
 
   test('of single blog list is the likes of that single blog', () => {
     expect(listHelper.totalLikes(singleBlogList)).toBe(9);
+  });
+  test('of a list with multiple blogs', () => {
+    expect(listHelper.totalLikes(blog)).toBe(10);
   });
 });
