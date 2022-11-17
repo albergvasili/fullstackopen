b7894883ec9cde24c87fb9f705ab4bd7047a1e1e
albergvasili <albergvasili@yandex.com>
2022-11-17 18:45:04 -0500

    Test 'totalLikes' function (empty list)
    -Write test for empty list

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -5,3 +5,11 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe('total likes', () => {
+  const emptyList = [];
+
+  test('of empty list is zero', () => {
+    expect(listHelper.totalLikes(emptyList)).toBe(0);
+  });
+});
