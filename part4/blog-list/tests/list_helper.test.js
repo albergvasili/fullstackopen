20eb8ade667d2e328ff02b08ead443ab11508402
albergvasili <albergvasili@yandex.com>
2022-11-17 19:56:04 -0500

    Refactor code
    -Move variables to the global scope

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,35 +1,35 @@
 const listHelper = require('../utils/list_helper');
 
+const emptyList = [];
+const singleBlogList = [
+  {
+    title: 'Loneliness',
+    authors: 'Sola No',
+    url: 'Ouais',
+    likes: 9
+  }
+];
+const twoBlogList = [
+  {
+    title: 'String',
+    authors: 'String',
+    url: 'String',
+    likes: 3
+  },
+  {
+    title: 'Guten tag',
+    authors: 'Tralali',
+    url: 'Line',
+    likes: 7
+  }
+];
+
 test('dummy returns one', () => {
-  const blogs = [];
-  const result = listHelper.dummy(blogs);
+  const result = listHelper.dummy(twoBlogList);
   expect(result).toBe(1);
 });
 
 describe('total likes', () => {
-  const emptyList = [];
-  const singleBlogList = [
-    {
-      title: 'Loneliness',
-      authors: 'Sola No',
-      url: 'Ouais',
-      likes: 9
-    }
-  ];
-  const blog = [
-    {
-      title: 'String',
-      authors: 'String',
-      url: 'String',
-      likes: 3
-    },
-    {
-      title: 'Guten tag',
-      authors: 'Tralali',
-      url: 'Line',
-      likes: 7
-    }
-  ];
 
   test('of empty list is zero', () => {
     expect(listHelper.totalLikes(emptyList)).toBe(0);
@@ -38,7 +38,8 @@ describe('total likes', () => {
   test('of single blog list is the likes of that single blog', () => {
     expect(listHelper.totalLikes(singleBlogList)).toBe(9);
   });
+
   test('of a list with multiple blogs', () => {
-    expect(listHelper.totalLikes(blog)).toBe(10);
+    expect(listHelper.totalLikes(twoBlogList)).toBe(10);
   });
 });
