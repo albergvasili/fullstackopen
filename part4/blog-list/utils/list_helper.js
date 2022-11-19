2dbec4ecf6298eb580d67a8a8bc13efa405f9736
albergvasili <albergvasili@yandex.com>
2022-11-18 19:25:18 -0500

    Define 'favoriteBlog' function
    -Use reduce to check amount of likes of each blog. Return the blog with
    most likes
    -Export new function

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -10,7 +10,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let topBlog = {};
+  const reducer = (x, blog) => {
+    if (blog.likes > x) {
+      topBlog = blog;
+      return blog.likes;
+    } else {
+      return x;
+    }
+  };
+  blogs.reduce(reducer, 0);
+  return topBlog;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
