4ed27a2c436a749811a4f5adf30717347741b0eb
albergvasili <albergvasili@yandex.com>
2022-12-05 18:58:49 -0500

    Run tests in a dedicated database
    -Modify utils/config to run on a specific database based on the node
    environment variable

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
-const mongoURL= process.env.MONGO_URL;
 const PORT = process.env.PORT;
+
+const mongoURL= process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGO_URL
+  : process.env.MONGO_URL;
 
 module.exports = {
   mongoURL,
