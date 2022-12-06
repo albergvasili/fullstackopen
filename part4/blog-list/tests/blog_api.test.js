83b1607873baa36121aadb6cd7c8d2e809ef94bd
albergvasili <albergvasili@yandex.com>
2022-12-06 18:06:31 -0500

    Test GET request to /api/blogs
    -Verify that the request returns the right amount of posts
    
    Exercise 4.8 Blog list tests step1

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const helper = require('../utils/list_helper');
 
 const api = supertest(app);
 
 test('GET request returns the correct amount of posts', async () => {
   const res = await api.get('/api/blogs');
 
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  expect(res.body).toHaveLength(helper.multiBlogList.length);
 });
 
 afterAll(() => {
