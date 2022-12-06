d4a578ba298539ed7bab6d7499ff6bfc364793a4
albergvasili <albergvasili@yandex.com>
2022-12-06 18:10:17 -0500

    Initialize database before tests
    -Use the beforeEach() function to clear database before each test run

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
 const helper = require('../utils/list_helper');
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.multiBlogList);
+});
 
 test('GET request returns the correct amount of posts', async () => {
   const res = await api.get('/api/blogs');
