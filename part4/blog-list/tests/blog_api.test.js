5f2e634c692db13257ca5926f0d261b92708b1aa
albergvasili <albergvasili@yandex.com>
2022-12-07 18:34:56 -0500

    Check if likes gets assigned 0 when missing
    -Use 'not.toBeDefined' to check if requests have a 'likes' property
    -Check if 'likes' has the value 0 after data is sent to /api/blogs
    -Add missing semi-colons
    
    Exercise 4.9 Blog list tests step4

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -30,14 +30,30 @@ test('POST request succesfully creates a new blog post', async () => {
     .post('/api/blogs')
     .send(post)
     .expect(201)
-    .expect('Content-Type', /application\/json/)
+    .expect('Content-Type', /application\/json/);
 
   const res = await api.get('/api/blogs');
 
-  const titles = res.body.map(blog => blog.title)
+  const titles = res.body.map(blog => blog.title);
 
   expect(res.body).toHaveLength(helper.multiBlogList.length + 1);
   expect(titles).toContain(helper.singleBlogList[0].title);
+});
+
+test('"Likes" property defaults to 0 if missing', async () => {
+  const post = helper.postWithoutLikes;
+
+  expect(post.likes).not.toBeDefined();
+
+  await api
+    .post('/api/blogs')
+    .send(post)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const res = await api.get('/api/blogs');
+
+  expect(res.body[res.body.length - 1].likes).toEqual(0);
 });
 
 afterAll(() => {
