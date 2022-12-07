6985df0875ceb8fffa93478b1bb752cde8ec7700
albergvasili <albergvasili@yandex.com>
2022-12-06 19:33:52 -0500

    Test POST route
    -Check that the length of the blogs is increased by one
    -Checck that the content of the post is saved correctly
    
    Exercise 4.10 Blog list tests step3

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -23,6 +23,23 @@ test('Unique identifier property of a post is named "id"', async () => {
   expect(res.body[0].id).toBeDefined();
 });
 
+test('POST request succesfully creates a new blog post', async () => {
+  const post = helper.singleBlogList[0];
+
+  await api
+    .post('/api/blogs')
+    .send(post)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const res = await api.get('/api/blogs');
+
+  const titles = res.body.map(blog => blog.title)
+
+  expect(res.body).toHaveLength(helper.multiBlogList.length + 1);
+  expect(titles).toContain(helper.singleBlogList[0].title);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
